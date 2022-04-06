e37ec87c0bebdfb846e7be81ba7c47f9207f58af
Alireza Vafi <ar.vafi@gmail.com>
2022-04-06 17:42:05 +0430

    removed custom response writer when Health Check UI is not added

--- src/Core/AppHealthCheckOptionBuilder.cs
+++ src/Core/AppHealthCheckOptionBuilder.cs
@@ -286,66 +286,7 @@ namespace HealthCheck.AspNetCore.Plus
 
         private static Func<HttpContext, HealthReport, Task> GetResponseWriter(AppHealthCheckOptions options)
         {
-            if (options.AddUi)
-                return UIResponseWriter.WriteHealthCheckUIResponse;
-
-            return WriteResponse;
-        }
-
-        private static Task WriteResponse(HttpContext context, HealthReport healthReport)
-        {
-            context.Response.ContentType = "application/json; charset=utf-8";
-
-            var options = new JsonWriterOptions {Indented = true};
-
-            using var memoryStream = new MemoryStream();
-            using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
-            {
-                jsonWriter.WriteStartObject();
-                jsonWriter.WriteString("status", healthReport.Status.ToString());
-                jsonWriter.WriteStartObject("results");
-
-                foreach (var healthReportEntry in healthReport.Entries)
-                {
-                    jsonWriter.WriteStartObject(healthReportEntry.Key);
-                    jsonWriter.WriteString("status",
-                        healthReportEntry.Value.Status.ToString());
-                    jsonWriter.WriteString("description",
-                        healthReportEntry.Value.Description);
-                    jsonWriter.WriteString("duration",
-                        healthReportEntry.Value.Duration.ToString());
-                    if (healthReportEntry.Value.Exception != null)
-                    {
-                        jsonWriter.WriteString("exception",
-                            healthReportEntry.Value.Exception.Message.ToString());
-                    }
-                    
-                    jsonWriter.WriteStartArray("tags");
-                    foreach (var tag in healthReportEntry.Value.Tags)
-                    {
-                        jsonWriter.WriteStringValue(tag);
-                    }
-                    jsonWriter.WriteEndArray();
-                    
-                    jsonWriter.WriteStartObject("data");
-                    foreach (var item in healthReportEntry.Value.Data)
-                    {
-                        jsonWriter.WritePropertyName(item.Key);
-
-                        JsonSerializer.Serialize(jsonWriter, item.Value,
-                            item.Value?.GetType() ?? typeof(object));
-                    }
-
-                    jsonWriter.WriteEndObject();
-                    jsonWriter.WriteEndObject();
-                }
-
-                jsonWriter.WriteEndObject();
-                jsonWriter.WriteEndObject();
-            }
-
-            return context.Response.WriteAsync(
-                Encoding.UTF8.GetString(memoryStream.ToArray()));
+            return UIResponseWriter.WriteHealthCheckUIResponse;
         }
     }
 }
